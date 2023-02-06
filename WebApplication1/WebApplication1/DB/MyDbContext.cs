a0ee87f1bda984ebde3fe7abb54f45c307e322d3
MARDOM\mcrodriguez <marckorv87@gmail.com>
2023-02-06 15:22:04 -0400

    First Commit

---
+++ WebApplication1/WebApplication1/DB/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.DB
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ArticleMatrix>? ArticleMatrices { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Feed.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class Feed
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string FeedType { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime PubDate { get; set; }
+
+        public Feed()
+        {
+            Link = "";
+            Title = "";
+            FeedType = "";
+            Author = "";
+            Content = "";
+            PubDate = DateTime.Today;
+        }
+    }
+}
